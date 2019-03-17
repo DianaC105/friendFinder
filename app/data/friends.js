ea0f37712f9205cd95c43d9e297498020e201906
Diana <carrillo.diana105@gmail.com>
2019-03-17 00:35:11 -0400

    changed API routes/jv

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,8 +2,8 @@
 
 var friendsArray = [
 {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name":"Yuri",
+    "photo":"https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
     "scores":[
       5,
       1,
@@ -18,8 +18,8 @@ var friendsArray = [
     ]
 },
 {
-  "name":"Andrea",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "name":"Andrew",
+  "photo":"https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
   "scores":[
     2,
     1,
@@ -32,8 +32,41 @@ var friendsArray = [
     4,
     5
   ]
+},
+{
+  "name":"Matt",
+  "photo":"https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  "scores":[
+    2,
+    1,
+    4,
+    5,
+    5,
+    3,
+    4,
+    5,
+    5,
+    1
+  ]
+},
+{
+"name":"Yadira",
+"photo":"https://images.pexels.com/photos/761977/pexels-photo-761977.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+"scores":[
+  1,
+  1,
+  4,
+  4,
+  5,
+  2,
+  2,
+  1,
+  4,
+  5
+]
 }
-];
+]
+
 
 //Making File Accessible 
 module.export = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,7 +5,7 @@
 // ===============================================================================
 
 var friendsArray = require("../data/friends");
-var path = require ("path");
+
 
 // ===============================================================================
 // ROUTING
@@ -36,35 +36,53 @@ module.exports = function(app) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body parsing middleware
+    console.log (req.body.scores);
+
 
     var userData =req.body;
-    var totalDifference =1000;
-    var newFriend = userData.scores;
-    var matchName =" ";
+
+
+    for(var i = 0; i< userData.scores.length; i++){
+
+      userData.scores[i]= parseInt(userData.scores[i]);
+    }
+    //var totalDifference =1000;
+    var newFriend = 0;
+    var minimumDiff = 40;
 
 
     // loop thru friendsArray first - this will look thru the existing user and new friend 
     
 
     for (var i = 0; i < friendsArray.length; i++){
-      diff= 0;
+      var totalDifference= 0;
     
-    for (var j = 0; j < newFriend.length; j++){
+    for (var j = 0; j < userData.length; j++){
     //finding the difference between the answers of friend and New friend 
-      diff += Math.abs(friendsArray[i].scores-newFriend[j])
-    };
+      var diff = Math.abs(userData.scores[j]- friendsArray[i].scores[j]);
+      totalDifference += diff;
+    }
+
+    if(totalDifference <minimumDiff){
+      newFriend = i;
+      minimumDiff = totalDifference;
+    }
+  }
+
+
     // pushing the different between the scores of the data 
     //userData.push(diff);
 
-    if (diff < totalDifference){
-      totalDifference= diff 
-      matchName =friendsArray[i].name
-    }
+    // if (diff < totalDifference){
+    //   totalDifference= diff 
+    //   matchName =friendsArray[i].name
+    // }
     // put new friend into the friendsArray 
-   friendsArray.push(userData);
+   userData.push(userData);
 
-   res.json(matchName);
+   res.json(userData[newFriend]);
+  //  res.end("hello world");
 
-  }
-  })
-  }
+ // }
+  });
+  };
